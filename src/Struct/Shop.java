152ecb89ec2be0e1c58c45c473a8b8f99b19f86b
Oleksandr <avpavlov96@gmail.com>
2016-12-27 16:16:46 +0300

    there are many racks

--- src/Struct/Shop.java
+++ src/Struct/Shop.java
@@ -8,25 +8,11 @@ import java.util.List;
  * Created by Oleksandr on 27.12.2016.
  */
 public class Shop {
-
-    private RackForClothes rackForClothes;
-
+    
     private List<RackForClothes> rackForClothesList;
-
-    public Shop(RackForClothes rackForClothes) {
-        this.rackForClothes = rackForClothes;
-    }
 
     public Shop(List<RackForClothes> rackForClothesList) {
         this.rackForClothesList = rackForClothesList;
-    }
-
-    public RackForClothes getRackForClothes() {
-        return rackForClothes;
-    }
-
-    public void setRackForClothes(RackForClothes rackForClothes) {
-        this.rackForClothes = rackForClothes;
     }
 
     public List<RackForClothes> getRackForClothesList() {
@@ -40,7 +26,6 @@ public class Shop {
     @Override
     public String toString() {
         return "Shop{" +
-                "rackForClothes=" + rackForClothes +
                 ", rackForClothesList=" + rackForClothesList +
                 '}';
     }
